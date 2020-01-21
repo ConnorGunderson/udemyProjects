77756e1882c033e9ac544fbb9f32e4d89b0f07ac
Connor Gunderson <gundy@pop-os.localdomain>
2020-01-21 16:07:42 -0500

    finished Project26

--- webDeveloperBootcamp/Project26/Part03/app.js
+++ webDeveloperBootcamp/Project26/Part03/app.js
@@ -1,21 +1,31 @@
 const express = require('express')
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs")
 
+let friends = ["Tyler", "Alyssa", "Emily", "Braden", "Eric"]
+
+// form
+app.post("/addFriend", function(req, res){
+    let newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("/friends")
+});
 // home dir
 app.get("/", function(req, res){
     res.render("home");
 })
 
-// form
 app.get("/friends", function(req, res){
-    let friends = ["Tyler", "Alyssa", "Emily", "Braden", "Eric"]
     res.render("friends", {friends: friends})
 })
-
 
 app.listen(3000, process.env.IP, function(){
     console.log("working clean");
 });
+
+// form requires action and method
+// when you put an input, add a variable name.
