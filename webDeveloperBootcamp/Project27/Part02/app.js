b516e0e4a42b95c57a05ce91b763c2c6f473df63
Connor Gunderson <gundy@pop-os.localdomain>
2020-01-22 14:27:12 -0500

    working on theatre project

--- webDeveloperBootcamp/Project27/Part02/app.js
+++ webDeveloperBootcamp/Project27/Part02/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser")
 const rp = require('request-promise')
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
--- webDeveloperBootcamp/Project27/Part03/app.js
+++ webDeveloperBootcamp/Project27/Part03/app.js
@@ -1,3 +1,34 @@
 const express = require('express');
 const app = express();
 const rp = require("request-promise");
+// const request = require('request')
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+// doing a search route
+app.get('/', ())
+
+app.get('/results', (req, res) => {
+    rp('http://www.omdbapi.com/?s=iowa&apikey=thewdb')
+        .then((b) => {
+            let parseData = JSON.parse(b)
+            res.render('results', {parseData: parseData});
+        })
+        .catch((err) => {
+            console.log('Error:', err)
+        })
+})
+
+// request('http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb', (err, res, b) => {
+//     eval(require('locus'))
+//     if (!err && res.statusCode == 200) {
+//         console.log('nice');
+        
+//     } else {
+//         console.log('Errer', err);
+        
+//     }
+// });
+
+app.listen(3000, process.env.IP, () => {})
