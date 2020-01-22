71fb34a228bc354b11f14b94f62cc341fc864215
Connor Gunderson <gundy@pop-os.localdomain>
2020-01-22 12:12:48 -0500

    added warmups

---
+++ warmups/warmup01/README.md
@@ -0,0 +1,1 @@
+Practicing basic routing, GET and POST requests, npm management. setting engine wiew and using ejs
---
+++ warmups/warmup01/app.js
@@ -0,0 +1,48 @@
+const request = require("request")
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+
+// calling API's
+request("http://www.google.com", function(err, res, b){
+    if (!err && res.statusCode == "200"){
+        console.log("nice")
+    } else {
+        console.log("error", err)
+        
+    }
+})
+
+let animals = [
+    {animal: "cow", sound: "moo!"},
+    {animal: "cat", sound: "meow!"},
+    {animal: "falcon", sound: "punch!"} 
+]
+let animalList = ['cow', 'dog', 'cat']
+// list all animals within the array
+app.get("/", function(req, res){
+    res.render("home", {animalList: animalList, 
+        animals: animals});
+})
+
+// render speech
+app.get("/x/:speak", function(req, res){
+    let speak = req.params.speak
+    res.render("speak", {speak: speak})
+})
+
+// push new animal with POST to
+app.post("/newAnimal", (req, res) => {
+    let nAnimal = req.body.newAnimal
+    animalList.push(nAnimal)
+    res.redirect("/")
+})
+
+// listening port
+app.listen(3000, process.env.IP, function(){
+    console.log("init")
+})
